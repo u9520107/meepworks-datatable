1afcc401f01889cf3ec9d258113d96dfaf056de8
Jack Tzu-Chieh Huang <tcjack@meepshop.com>
2015-07-20 15:01:03 +0800

    backup

--- test-server/app/app.js
+++ test-server/app/app.js
@@ -2,10 +2,18 @@ import React from 'react';
 import Application from 'meepworks/application';
 
 import 'normalize.css/normalize.css!';
+import 'fixed-data-table/dist/fixed-data-table.css!';
 
-import Table from '../../build/components/table';
-import Column from '../../build/components/column';
+/*
+ *import Table from '../../build/components/table';
+ *import Column from '../../build/components/column';
+ */
 
+import { Table, Column } from 'fixed-data-table';
+
+class TableNew extends Table {
+
+}
 
 const data = Array.apply(null, new Array(300)).map((n, idx) => {
   return {
@@ -21,33 +29,22 @@ export default class App extends Application {
     super(props, context);
     this.state =  {
       width: 800,
-      height: 600
+      height: 600,
+      expansions: {}
     };
   }
   render() {
+
+    /*
+     *return (
+     *  <div>Hello</div>
+     *);
+     */
     return (
       <div style={{
         position: 'absolute'
       }}>
-      <div key="params" >
-        height:<input key="height" value={this.state.height} onChange={(e) => {
-          this.setState({
-            height: parseInt(e.currentTarget.value)
-          });
-        }}/> <br />
-      width: <input key="width" value={this.state.width} onChange={e => {
-        this.setState({
-          width: parseInt(e.currentTarget.value)
-        });
-      }}/>
-        </div>
-        <div
-          key="table"
-          style={{
-            border: '1px solid lightgray',
-            position: 'absolute'
-        }}>
-        <Table
+        <TableNew
           rowGetter={idx => {
             return data[idx];
           }}
@@ -55,26 +52,47 @@ export default class App extends Application {
           width={this.state.width}
           height={this.state.height}
           headerHeight={34}
+          rowHeight={34}
+          rowHeightGetter={(idx) => {
+            if(this.state.expansions[idx]) {
+              return 100;
+            }
+            return 34;
+          }}
+          onRowClick={(e, idx, data) => {
+            let expansions = this.state.expansions;
+            expansions[idx] = !expansions[idx];
+            this.setState({
+              expansions
+            });
+          }}
           >
           <Column
             dataKey="id"
             label="ID"
+            width={100}
+            flexGrow={1}
           />
           <Column
             dataKey="name"
             label="Name"
+            width={100}
+            flexGrow={1}
             fixed={true}
           />
           <Column
             dataKey="fieldA"
             label="Field A"
+            width={100}
+            flexGrow={1}
           />
           <Column
             dataKey="fieldB"
             label="Field B"
+            width={100}
+            flexGrow={1}
           />
-        </Table>
-      </div>
+        </TableNew>
     </div>
     );
   }
